a7d06dedb4b3f8fa1d47e53a68b99f2a248ca9f8
Marcin <marcin@marcinbazylak.com>
2020-06-12 11:27:22 +0200

    Zrobiony podstawowy layout

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.layout')
+
+@section('content')
+<aside>
+   <a href="/admin/albums">
+      <button class="title">Albumy</button>
+   </a>
+   <a href="/admin/photos">
+      <button class="title">Zdjęcia</button>
+   </a>
+   <a href="/admin/addPhoto">
+      <button class="title">Dodaj zdjęcie</button>
+   </a>
+</aside>
+
+<main>
+
+ADMINISTRACJA
+
+</main>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/img.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Marcin Bazylak Galeria</title>
+</head>
+<body>
+
+   <div class="container">
+      <img src="img/niko.jpg" alt="Nikodem" class="top-img">
+
+      <div class="wrapper">
+
+         <div class="grid">
+
+            <header>
+
+               <h1>MARCIN BAZYLAK - GALERIA</h1>
+               
+            </header>
+
+               @yield('content')
+
+            <footer>
+
+               Copyright by Marcin Bazylak 2019. All Rights Reserved.
+
+            </footer>
+            
+         </div>
+
+      </div>
+      
+   </div>
+   
+
+   <script>
+
+const target = document.getElementById("target");
+
+document.addEventListener("wheel", function(e){
+  // prevent the default scrolling event
+  e.preventDefault();
+
+  // scroll the div
+  target.scrollBy(e.deltaX, e.deltaY);
+});
+
+   </script>
+</body>
+</html>
